a6a8f65dad0a256133af80ec3d310c4059f89bfc
romam20001888 <romanrogankov2012@gmail.com>
2023-02-17 20:32:40 +0300

    добавление задач

--- pages/AddTiket.jsx
+++ pages/AddTiket.jsx
@@ -1,13 +1,67 @@
 import * as React from 'react';
-import {StyleSheet,View,TextInput,Text} from 'react-native';
+import {StyleSheet,View,TextInput,Text,ScrollView} from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown'
 
 const AddTiket = ({navigation}) => {
+    const countries = ["Egypt", "Canada", "Australia", "Ireland"]
+    const critical = ["Средняя", "Высокая", "Авария"]
+    const category = ["Общие вопросы", "Работа по дизайну", "Работа по программированию", "Работа по наполнению", "Ошибки"]
+    
     return (
-        <View style={styles.container}>
-            <Text>
-                Страница в разработке
-            </Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <TextInput
+                style={styles.inputText} 
+                placeholder='Название задачи'
+            />
+            <View style={styles.containerTextTextarea} >
+                <Text>Nen будет поле описапния</Text>
+            </View>
+            <SelectDropdown
+                buttonStyle={styles.inputTextSelect}
+                buttonTextStyle={styles.inputTextSelectText}
+                data={countries}
+                onSelect={(selectedItem, index) => {
+                    console.log(selectedItem, index)
+                }}
+                defaultButtonText="Привязать к проекту"
+                buttonTextAfterSelection={(selectedItem, index) => {
+                    return selectedItem
+                }}
+                rowTextForSelection={(item, index) => {
+                    return item
+                }}
+            />
+            <SelectDropdown
+                buttonStyle={styles.inputTextSelect}
+                buttonTextStyle={styles.inputTextSelectText}
+                data={critical}
+                onSelect={(selectedItem, index) => {
+                    console.log(selectedItem, index)
+                }}
+                defaultButtonText="Критичность"
+                buttonTextAfterSelection={(selectedItem, index) => {
+                    return selectedItem
+                }}
+                rowTextForSelection={(item, index) => {
+                    return item
+                }}
+            />
+            <SelectDropdown
+                buttonStyle={styles.inputTextSelect}
+                buttonTextStyle={styles.inputTextSelectText}
+                data={category}
+                onSelect={(selectedItem, index) => {
+                    console.log(selectedItem, index)
+                }}
+                defaultButtonText="Категория"
+                buttonTextAfterSelection={(selectedItem, index) => {
+                    return selectedItem
+                }}
+                rowTextForSelection={(item, index) => {
+                    return item
+                }}
+            />
+        </ScrollView>
     );
 };
 
@@ -15,12 +69,40 @@ const AddTiket = ({navigation}) => {
 
 
 const styles = StyleSheet.create({
+    inputText:{
+        paddingVertical:5,
+        paddingHorizontal:10,
+        borderWidth: 1,
+        borderRadius:20,
+        marginBottom:20,
+        width:"100%"
+    },
+    containerTextTextarea:{
+        marginBottom:20,
+        width:"100%",
+    },
+    inputTextTextarea:{
+        width:"100%",
+    },
+    inputTextSelect:{
+        height:"auto",
+        paddingVertical:5,
+        paddingHorizontal:10,
+        borderWidth: 1,
+        borderRadius:20,
+        marginBottom:20,
+        backgroundColor:"#17aaff",
+        color:"white",
+        width:"100%"
+    },
+    inputTextSelectText:{
+        color:"white",
+    },
     
     container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#fff',
+        width:"100%",
+        padding:10,
+        backgroundColor: '#fff',
     },
 });
   
