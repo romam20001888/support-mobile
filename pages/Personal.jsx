6722eb6ec93f71e0dc9a528576d1abb985febbc8
romam20001888 <romanrogankov2012@gmail.com>
2023-02-17 22:31:42 +0300

    Личный кабинет

--- pages/Personal.jsx
+++ pages/Personal.jsx
@@ -1,38 +1,50 @@
 import * as React from 'react';
-import {StyleSheet,ScrollView,TextInput,Text} from 'react-native';
+import {StyleSheet,ScrollView,TextInput,Text,TouchableOpacity} from 'react-native';
 
 const Personal = ({navigation}) => {
+    const [taskError, onChangeTaskError] = React.useState("");
     return (
-        <ScrollView style={styles.container}>
-            <TextInput
-                style={styles.inputText} 
-                placeholder='Имя'
-            />
-            <TextInput
-                style={styles.inputText} 
-                placeholder='Фамилия'
-            />
-            <TextInput
-                style={styles.inputText} 
-                placeholder='Отчество'
-            />
-            <TextInput
-                style={styles.inputText} 
-                placeholder='E-Mail'
-            />
-            <TextInput
-                style={styles.inputText} 
-                placeholder='Логин (мин. 3 символа)'
-            />
-            <TextInput
-                style={styles.inputText} 
-                placeholder='Новый пароль'
-            />
-            <TextInput
-                style={styles.inputText} 
-                placeholder='Подтверждение нового пароля'
-            />
-        </ScrollView>
+        <>
+            <ScrollView style={styles.container}>
+                <TextInput
+                    style={styles.inputText} 
+                    placeholder='Имя'
+                />
+                <TextInput
+                    style={styles.inputText} 
+                    placeholder='Фамилия'
+                />
+                <TextInput
+                    style={styles.inputText} 
+                    placeholder='Отчество'
+                />
+                <TextInput
+                    style={styles.inputText} 
+                    placeholder='E-Mail'
+                />
+                <TextInput
+                    style={styles.inputText} 
+                    placeholder='Логин (мин. 3 символа)'
+                />
+                <TextInput
+                    style={styles.inputText} 
+                    placeholder='Новый пароль'
+                />
+                <TextInput
+                    style={styles.inputText} 
+                    placeholder='Подтверждение нового пароля'
+                />
+            </ScrollView>
+            <Text style={styles.containerError}>{taskError}</Text>
+            <TouchableOpacity 
+                onPress={async () =>{
+                   
+                }}
+                style={styles.containerSaveUser}
+            >
+                <Text style={styles.containerSaveUserText}>Сохранить</Text>
+            </TouchableOpacity>
+        </>
     );
 };
 
@@ -40,7 +52,30 @@ const Personal = ({navigation}) => {
 
 
 const styles = StyleSheet.create({
-    
+    containerError:{
+        color:"red",
+        marginBottom:50,
+        backgroundColor: '#fff',
+        textAlign:"center"
+    },
+    containerSaveUserText: {
+        fontSize:20,
+        padding:0,
+        margin:0,
+        color:"white"
+    },
+    containerSaveUser: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position:"absolute",
+        bottom:0,
+        width:"100%",
+        height:50,
+        paddingBottom:5,
+        backgroundColor:"#17aaff",
+        right:0
+    },
     container: {
         width:"100%",
         padding:10,
